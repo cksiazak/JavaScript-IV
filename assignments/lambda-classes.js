d8e5e63beb1cbcf2d36f99d143a51adaef8a578c
Christopher Ksiazak <cksiazak@gmail.com>
2019-05-29 22:04:41 -0600

    created an Instructor class with methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,19 @@ class Person {
         return `Hello, my name is ${this.name}, I am from ${this.location}`
     }
 }
+
+class Instructor extends Person {
+    constructor(InstructAttrs) {
+        super(InstructAttrs);
+        this.specialty = InstructAttrs.specialty;
+        this.favLanguage = InstructAttrs.favLanguage;
+        this.catchPhrase = InstructAttrs.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}`
+    }
+    grade(student, subject) {
+        return `${student} receives a pergect score on ${subject}`
+    }
+}
